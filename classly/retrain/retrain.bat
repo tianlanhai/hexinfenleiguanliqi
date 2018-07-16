54f927ab74c6e9052d7c0ba30dbfcdf827aa365f
tianlanhai <zhaohj@vosung.cn>
2018-07-16 11:14:35 +0800

    分类核心

---
+++ classly/retrain/retrain.bat
@@ -0,0 +1,10 @@
+python E:/Python/tensorflow-master/tensorflow/examples/image_retraining/retrain.py ^
+--bottleneck_dir bottleneck ^
+--how_many_training_steps 400 ^
+--model_dir E:/Python/classly/inception_model/ ^
+--output_graph output_graph.pb ^
+--output_labels output_labels.txt ^
+--image_dir data/train/
+pause
+
+
